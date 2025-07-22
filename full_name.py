8337a7dd15c3f5fe88ef5b497f899a5b84c05f26
Afraz <afrazabdulhat@gmail.com>
2025-07-22 11:26:44 -0700

    first commit

---
+++ full_name.py
@@ -0,0 +1,47 @@
+first_name = "ada" 
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+print("Python")
+print("\tPython")
+print("languages:\nPython\nC\nJavascript")
+print("\tPython")
+print("Languages:\n\tPython\n\tC\tJavascript")
+print("   favorites_Language   ".lstrip().rstrip())
+print("  python  ".lstrip().rstrip())
+print("  Javascript  ".lstrip().rstrip())
+print(" C++ ".lstrip().rstrip())
+print(" Javascript ".rstrip().lstrip())
+print("Abdul ".rstrip())
+print("Aysha ".rstrip())
+print("Afsar ".rstrip())
+print("Afraz ".rstrip())
+print("Amira ".rstrip())
+print("My moms cooking is the best in the world it's better than restraunt food.")
+print("My dads coding can never be compared to any one else he's the best at coding.")
+print("Afsar is the smartest 8th and 9th grader in the world he's crazy smart.")
+print("Amira is the best singer and dancer in the whole world she lights up our house like light.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+      
+
+
+      
+ 
